bf6a5e704330e451f200f891c3c55386725f8f1c
degreane <degreane@gmail.com>
2023-05-16 12:20:55 +0300

    Boosted Auto StartUp Scripts with check ups

--- StartMongoDB.bash
+++ StartMongoDB.bash
@@ -1,4 +1,4 @@
 [ `systemctl is-active redis.service` == 'active' ] || sudo systemctl start redis.service
 [ `systemctl is-active docker.service` == 'active' ] || sudo systemctl start docker.service
-docker start giacomMongo 
-forever start -a -l forever.log -o out.log -e err.log ./bin/www
+[ `docker inspect giacomMongo | jq -a -M '.[0]["State"]["Running"] '` == 'true' ] ||  docker start giacomMongo 
+[ `ps -ef | grep node | grep GiaComMediaServer | wc -l` -eq 1 ] || forever start -a -l forever.log -o out.log -e err.log ./bin/www
