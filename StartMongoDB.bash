3909f96b23f8072cb18f997b4e557cb5729d4d7f
degreane <degreane@gmail.com>
2023-05-16 12:25:12 +0300

    restart server gracefully

--- StartMongoDB.bash
+++ StartMongoDB.bash
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
+## Do Redis 
 [ `systemctl is-active redis.service` == 'active' ] || sudo systemctl start redis.service
+
+## Do Docker
 [ `systemctl is-active docker.service` == 'active' ] || sudo systemctl start docker.service
+
+## Start Database
 [ `docker inspect giacomMongo | jq -a -M '.[0]["State"]["Running"] '` == 'true' ] ||  docker start giacomMongo 
-[ `ps -ef | grep node | grep GiaComMediaServer | wc -l` -eq 1 ] || forever start -a -l forever.log -o out.log -e err.log ./bin/www
+
+## Start/Restart server
+[ `ps -ef | grep node | grep GiaComMediaServer | wc -l` -eq 1 ] && forever restart 0 || forever start -a -l forever.log -o out.log -e err.log ./bin/www
