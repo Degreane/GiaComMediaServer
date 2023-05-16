f90ff837392d0193754320f8523b546546e2083d
degreane <degreane@gmail.com>
2023-05-16 11:51:53 +0300

    Enabled AutoStart Server Services

--- StartMongoDB.bash
+++ StartMongoDB.bash
@@ -1,1 +1,4 @@
+[ `systemctl is-active redis.service` == 'active' ] || sudo systemctl start redis.service
+[ `systemctl is-active docker.service` == 'active' ] || sudo systemctl start docker.service
 docker start giacomMongo 
+forever start -a -l forever.log -o out.log -e err.log ./bin/www
