6929882bb864f26b790a209048b03f49719e7020
degreane <degreane@gmail.com>
2023-06-07 23:52:24 +0300

    fixing dbErrors

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/GiaCom',{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useCreateIndex:true,
-    useFindAndModify:false,
+mongoose.connect('mongodb://127.0.0.1:27017/GiaCom',{
+    // useNewUrlParser:true,
+    // useUnifiedTopology: true,
+    // useCreateIndex:true,
+    // useFindAndModify:false,
     "auth": {
         "authSource": "admin"
       },
